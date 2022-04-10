d46f247ff8665c41e981deb2bc0e92eb8390c959
fxnnxc <bumjin@kaist.ac.kr>
2022-04-10 12:33:45 +0900

    updated the route

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -13,7 +13,7 @@ function MovieApp() {
     <Router>
       <Routes>
         <Route path="/movie/:id" element={<Detail/>}/>
-        <Route path="/" element={<Home/>}>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}>
         </Route>
       </Routes>
     </Router>
